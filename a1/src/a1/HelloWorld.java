7405138569b7118bbb31b459917952b53fad0c48
yoon <yoon@gmail.com>
2017-04-04 09:57:55 +0900

    added 'her world'

--- a1/src/a1/HelloWorld.java
+++ a1/src/a1/HelloWorld.java
@@ -1,5 +1,10 @@
 package a1;
 
 public class HelloWorld {
-
+	public static void main(String[] args) {
+	System.out.println("hello world");
+	System.out.println("hello his world");
+	System.out.println("hello her world");
+	System.out.println("hello your world");
+		}
 }
