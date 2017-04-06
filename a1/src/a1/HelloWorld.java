2f8e28202b0320350a695564c5917041adeb0a83
yoon <yoon@gmail.com>
2017-04-06 11:07:42 +0900

    a

--- a1/src/a1/HelloWorld.java
+++ a1/src/a1/HelloWorld.java
@@ -1,5 +1,9 @@
 package a1;
 
 public class HelloWorld {
-
+	public static void main(String[] args) {
+	System.out.println("hello world");
+	System.out.println("hello his world");
+	System.out.println("hello her world");
+	}
 }
--- a1/src/a1/a2.java
+++ a1/src/a1/a2.java
@@ -3,6 +3,7 @@ package a1;
 public class a2 {
 	public static void main(String[] args) {
 		System.out.println("Add Java file");
+		System.out.println("Hello...");
 	}
 	
 }
